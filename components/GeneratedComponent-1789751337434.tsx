f02a45a97a6b931290ce50f62e6404eaf9771a51
yooneeskoopaee <yooneeskoopaee@gmail.com>
2026-09-18 20:38:57 +0330

    GitHub access token

---
+++ components/GeneratedComponent-1789751337434.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function SaveButton() {
+  return (
+    <Button color="primary" size="default">
+      ثبت اطلاعات
+    </Button>
+  );
+}
